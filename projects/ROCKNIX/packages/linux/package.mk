4350d04d3bf919c02d95d6d9782a994fdff6f444
Stanislav <root-stas@ukr.net>
2025-11-07 00:47:30 +0200

    Update package.mk

--- projects/ROCKNIX/packages/linux/package.mk
+++ projects/ROCKNIX/packages/linux/package.mk
@@ -29,7 +29,6 @@ case ${DEVICE} in
     PKG_URL="https://gitlab.com/tjstyle/linux/-/archive/sdm845/${PKG_VERSION}-release/linux-sdm845-${PKG_VERSION}-release.tar.gz"
     PKG_PATCH_DIRS="${LINUX} ${DEVICE} default"
   ;;
-  *)
   H700)
     PKG_VERSION="6.18-rc4"
     PKG_URL="https://git.kernel.org/torvalds/t/linux-${PKG_VERSION}.tar.gz"
