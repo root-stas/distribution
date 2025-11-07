4a1b1c2bfc8444e6a1612a035d458f23d6e13e29
Stanislav <root-stas@ukr.net>
2025-11-07 18:06:09 +0200

    Update LLVM 21.1.5 Release
    Bump LLVM to 21.1.5

--- projects/ROCKNIX/packages/lang/llvm/package.mk
+++ projects/ROCKNIX/packages/lang/llvm/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="19.1.7"
-PKG_SHA256="82401fea7b79d0078043f7598b835284d6650a75b93e64b6f761ea7b63097501"
+PKG_VERSION="21.1.5"
+PKG_SHA256="1794be4bf974e99a3fe1da4b2b9b1456c02ae9479c942f365441d8d207bd650c"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="http://llvm.org/"
 PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION/-/}.src.tar.xz"
