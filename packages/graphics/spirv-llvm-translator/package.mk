1960e95eff54c2770b39d1150e149ea3e84f6702
Stanislav <root-stas@ukr.net>
2025-11-08 13:54:47 +0200

    Update package.mk

--- packages/graphics/spirv-llvm-translator/package.mk
+++ packages/graphics/spirv-llvm-translator/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="spirv-llvm-translator"
-PKG_VERSION="20.1.4"
-PKG_SHA256="f6c414f9384c8f68775d9461e55e7c492f01759f5a1754f30dad4cbfb049c7d7"
+PKG_VERSION="21.1.2"
+PKG_SHA256="8c91542b579a3b27b7aeae1db12004eb412c9ed9bdff0a29ee862c3551cadfe3"
 PKG_LICENSE="LLVM"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
 PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/v${PKG_VERSION}.tar.gz"
