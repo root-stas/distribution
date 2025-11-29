9da7385f1f4dcaff0b3c79c73674cb1870b54531
Stanislav <root-stas@ukr.net>
2025-11-29 07:45:42 +0200

    Dolphin LR Mainline

--- projects/ROCKNIX/packages/emulators/libretro/dolphin-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/dolphin-lr/package.mk
@@ -2,11 +2,12 @@
 # Copyright (C) 2022-present JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="dolphin-lr"
-PKG_VERSION="89a4df725d4eb24537728f7d655cddb1add25c18"
+PKG_VERSION=""
 PKG_LICENSE="GPLv2"
 PKG_DEPENDS_TARGET="toolchain libevdev libdrm ffmpeg zlib libpng lzo libusb"
 PKG_SITE="https://github.com/libretro/dolphin"
-PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/libretro/dolphin.git"
+PKG_GIT_SUBMODULE_DEPTH="1"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="Dolphin Libretro, a Gamecube & Wii emulator core for Retroarch"
 PKG_TOOLCHAIN="cmake"
