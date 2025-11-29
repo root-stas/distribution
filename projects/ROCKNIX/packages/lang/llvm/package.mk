9564a93de72e0406601dae8a2a62503c1babea91
Stanislav <root-stas@ukr.net>
2025-11-29 19:08:17 +0200

    LLVM pin to HEAD

--- projects/ROCKNIX/packages/lang/llvm/package.mk
+++ projects/ROCKNIX/packages/lang/llvm/package.mk
@@ -3,11 +3,10 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="21.1.6"
-PKG_SHA256="ae67086eb04bed7ca11ab880349b5f1ab6f50e1b88cda376eaf8a845b935762b"
+PKG_VERSION="HEAD"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="http://llvm.org/"
-PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION/-/}.src.tar.xz"
+PKG_URL="https://github.com/llvm/llvm-project.git"
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain llvm:host zlib"
 PKG_LONGDESC="Low-Level Virtual Machine (LLVM) is a compiler infrastructure."
