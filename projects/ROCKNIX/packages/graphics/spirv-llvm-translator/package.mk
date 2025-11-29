71c0b2bf9983308ff57ce57fbb6ad5e4fac92476
Stanislav <root-stas@ukr.net>
2025-11-29 23:57:24 +0200

    SPIRV LLVM pin to main

--- projects/ROCKNIX/packages/graphics/spirv-llvm-translator/package.mk
+++ projects/ROCKNIX/packages/graphics/spirv-llvm-translator/package.mk
@@ -2,9 +2,9 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="spirv-llvm-translator"
-PKG_VERSION="HEAD"
+PKG_VERSION="main"
 PKG_LICENSE="LLVM"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
-PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git"
+PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="SPIRV-LLVM-Translator"
 PKG_TOOLCHAIN="manual"
