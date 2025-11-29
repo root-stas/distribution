be1fca5fccdbee9fa674368c1d50d4ddae2fcbfd
Stanislav <root-stas@ukr.net>
2025-11-29 23:54:09 +0200

    SPIRV Tools pin to main

--- projects/ROCKNIX/packages/graphics/spirv-tools/package.mk
+++ projects/ROCKNIX/packages/graphics/spirv-tools/package.mk
@@ -6,10 +6,10 @@ PKG_NAME="spirv-tools"
 # The SPIRV-Tools pkg_version needs to match the compatible (known_good) glslang pkg_version.
 # https://raw.githubusercontent.com/KhronosGroup/glslang/${PKG_VERSION}/known_good.json
 # When updating glslang pkg_version please update to the known_good spirv-tools pkg_version.
-PKG_VERSION="HEAD"
+PKG_VERSION="main"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-Tools"
-PKG_URL="https://github.com/KhronosGroup/SPIRV-Tools.git"
+PKG_URL="https://github.com/KhronosGroup/SPIRV-Tools/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_DEPENDS_UNPACK="spirv-headers"
