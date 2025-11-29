f3695e218518afd5ea7f26e1c489f5a597911607
Stanislav <root-stas@ukr.net>
2025-11-29 18:22:59 +0200

    SPIRV-Headers mainline

--- projects/ROCKNIX/packages/graphics/spirv-headers/package.mk
+++ projects/ROCKNIX/packages/graphics/spirv-headers/package.mk
@@ -6,10 +6,10 @@ PKG_NAME="spirv-headers"
 # The SPIRV-Headers pkg_version needs to match the compatible (known_good) glslang pkg_version.
 # https://raw.githubusercontent.com/KhronosGroup/glslang/${PKG_VERSION}/known_good.json
 # When updating glslang pkg_version please update to the known_good spirv-headers pkg_version.
-PKG_VERSION="01e0577914a75a2569c846778c2f93aa8e6feddd"
+PKG_VERSION="HEAD"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-headers"
-PKG_URL="https://github.com/KhronosGroup/SPIRV-headers/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/KhronosGroup/SPIRV-headers.git"
 PKG_DEPENDS_HOST=""
 PKG_LONGDESC="SPIRV-Headers"
 PKG_TOOLCHAIN="manual"
