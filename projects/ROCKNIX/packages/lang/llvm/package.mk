6ad9e3bd9816bb6ac3e8f953828d4944d73a730a
Stanislav <root-stas@ukr.net>
2025-11-30 08:44:59 +0200

    set LLVM 21.1.6 due to Mesa fail build with LLVM 22

--- projects/ROCKNIX/packages/lang/llvm/package.mk
+++ projects/ROCKNIX/packages/lang/llvm/package.mk
@@ -3,10 +3,10 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="main"
+PKG_VERSION="21.1.6"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="http://llvm.org/"
-PKG_URL="https://github.com/llvm/llvm-project/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION/-/}.src.tar.xz"
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain llvm:host zlib"
 PKG_LONGDESC="Low-Level Virtual Machine (LLVM) is a compiler infrastructure."
