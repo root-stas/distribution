f5d8ec6e5444cd5e6d5c06bc41498a5ced38152d
Stanislav <root-stas@ukr.net>
2025-12-01 07:59:35 +0200

    SPIRV LLVM 21.1.3

--- projects/ROCKNIX/packages/graphics/spirv-llvm-translator/package.mk
+++ projects/ROCKNIX/packages/graphics/spirv-llvm-translator/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="spirv-llvm-translator"
-PKG_VERSION="main"
+PKG_VERSION="a79c852b36785588032a2dd7c39aa15ec4d24b20"
 PKG_LICENSE="LLVM"
 PKG_SITE="https://github.com/KhronosGroup/SPIRV-LLVM-Translator"
 PKG_URL="https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/${PKG_VERSION}.tar.gz"
