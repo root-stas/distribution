dafa4f7aeacbb2719cce7611238fbdef4c563cfb
Stanislav <root-stas@ukr.net>
2025-12-03 00:47:34 +0200

    Update vkmark package.mk

--- packages/graphics/vulkan/vkmark/package.mk
+++ packages/graphics/vulkan/vkmark/package.mk
@@ -2,10 +2,12 @@
 # Copyright (C) 2022-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="vkmark"
-PKG_VERSION="master"
+# PKG_VERSION="master"
+PKG_VERSION="next"
 PKG_LICENSE="LGPL-2.1-or-later"
 PKG_SITE="https://github.com/vkmark/vkmark"
-PKG_URL="https://github.com/vkmark/vkmark/archive/${PKG_VERSION}.tar.gz"
+# PKG_URL="https://github.com/vkmark/vkmark/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/root-stas/vkmark.git"
 PKG_DEPENDS_TARGET="toolchain glm assimp vulkan-loader"
 PKG_LONGDESC="Vulkan benchmark"
 PKG_BUILD_FLAGS="-parallel +speed"
