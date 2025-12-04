ff1da451ce68e6344db57e93d6bb35ecc2fbef57
Stanislav <root-stas@ukr.net>
2025-12-04 13:50:59 +0200

    Update package version and URL for mupen64plus-sa-video-glide64mk2

--- projects/ROCKNIX/packages/emulators/standalone/mupen64plus-sa/mupen64plus-sa-video-glide64mk2/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/mupen64plus-sa/mupen64plus-sa-video-glide64mk2/package.mk
@@ -4,10 +4,10 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="mupen64plus-sa-video-glide64mk2"
-PKG_VERSION="992b5942078fe77987e8c40bcd396f44be19be2b"
+PKG_VERSION="master"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/mupen64plus/mupen64plus-video-glide64mk2"
-PKG_URL="https://github.com/mupen64plus/mupen64plus-video-glide64mk2/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/mupen64plus/mupen64plus-video-glide64mk2.git"
 PKG_DEPENDS_TARGET="toolchain boost libpng SDL2 SDL2_net zlib freetype nasm:host mupen64plus-sa-core"
 PKG_LONGDESC="mupen64plus-video-glide64mk2"
 PKG_LONGDESC="Mupen64Plus Standalone Glide64 Video Driver"
